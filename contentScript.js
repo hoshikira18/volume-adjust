a416d6710b5d41e0c67514069d0b728cbe752f6a
@tsuki1803 <khuyen10a5k61@gmail,com>
2022-09-10 21:30:08 +0700

    commit by Khuyen

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,10 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.message === "hello") {
-    alert('done')
+    let mediaElements = document.querySelectorAll('video, audio')
+    for (let i = 0; i < mediaElements.length; i++) {
+      mediaElements[i].volume = message.volume
+      console.log(`Now volume: ${mediaElements[i].volume}`)
+    }
   }
 })
 
